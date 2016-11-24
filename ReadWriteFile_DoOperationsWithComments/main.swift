34ae5292aee56f22c9178db3a67c4e4de8e68ed1
Aleksandr Zholnerovskij <sasha@Aleksandrs-MacBook-Pro.local>
2016-11-24 22:30:04 +0200

    Initial Commit

---
+++ ReadWriteFile_DoOperationsWithComments/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ReadWriteFile_DoOperationsWithComments
+//
+//  Created by Aleksandr Zholnerovskij on 11/24/16.
+//  Copyright © 2016 Aleksandr Zholnerovskij. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
